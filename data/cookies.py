d2c81bb08e518a27f16bf48e1f53a6a891e42a2e
sdet0mis <sdet0mis@mail.ru>
2025-05-30 16:31:56 +0300

    fix get cookie function

--- data/cookies.py
+++ data/cookies.py
@@ -6,6 +6,6 @@ def save_cookie(cookie: dict) -> None:
         json.dump(cookie, file, indent=4)
 
 
-def get_cookie() -> dict:
+def get_cookie_from_file() -> dict:
     with open('data/cookie.json', 'r') as file:
         return json.load(file)
