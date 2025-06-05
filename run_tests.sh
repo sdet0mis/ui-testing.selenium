60a8e547302005999d124f37d48ea2da64ee96d1
sdet0mis <sdet0mis@mail.ru>
2025-06-05 13:14:04 +0300

    add autorun failed tests

--- run_tests.sh
+++ run_tests.sh
@@ -4,4 +4,4 @@ set -e
 
 java -jar grid/selenium-server-4.33.0.jar hub & \
 java -jar grid/selenium-server-4.33.0.jar node --hub http://localhost:4444 & \
-pytest -n ${1:-auto} --alluredir=allure-results
+pytest -n ${1:-auto} --reruns 2 --alluredir=allure-results
